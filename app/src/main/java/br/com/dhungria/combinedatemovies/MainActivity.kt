b02ae2668b73d7833175650a7bbe7f607cad9c63
danielhungria <danielhungria14@gmail.com>
2023-05-29 03:15:33 -0300

    feature: implemented ads

--- app/src/main/java/br/com/dhungria/combinedatemovies/MainActivity.kt
+++ app/src/main/java/br/com/dhungria/combinedatemovies/MainActivity.kt
@@ -2,16 +2,21 @@ package br.com.dhungria.combinedatemovies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import br.com.dhungria.combinedatemovies.databinding.ActivityMainBinding
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+
+    private lateinit var mAdView: AdView
 
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
@@ -20,7 +25,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
+        setupBannerAd()
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
@@ -29,5 +34,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    private fun setupBannerAd() {
+        binding.adViewBannerBottomMain.visibility = View.VISIBLE
+        mAdView = binding.adViewBannerBottomMain
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
     }
 }
